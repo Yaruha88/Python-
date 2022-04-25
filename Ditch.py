34d36c5bfca6650f666477162fa5cd0e128e9b66
Yaruha88 <77204010+Yaruha88@users.noreply.github.com>
2022-04-25 15:04:21 +0300

    Update Ditch.py

--- Ditch.py
+++ Ditch.py
@@ -1,13 +1,13 @@
 print('У нас есть что-то для тебя! Выбирай: ') 
 
 printStock(storage)
-bin = input('Что выбираешь? ')  
-q = int(input('Какое количество берешь? ')) 
+purchases = input('Что выбираешь? ')  
+quantity = int(input('Какое количество берешь? ')) 
 for i in storage: 
-    if i['name'] == bin: 
-        if q <= i['count']: 
-            i['count'] = i['count'] - q 
-            print(('Ты выбрал ') + (bin) + str(' в количестве ') + str(q) + str(' шт! ') + str('Осталось на складе '), + (i['count']), str('шт')) 
+    if i['name'] == purchases: 
+        if quantity <= i['count']: 
+            i['count'] = i['count'] - quantity 
+            print(('Ты выбрал ') + (purchases) + str(' в количестве ') + str(quantity) + str(' шт! ') + str('Осталось на складе '), + (i['count']), str('шт')) 
         else: 
             print('Слишком дохера, нет столько!') 
 
