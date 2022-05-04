36c6fec586ec95af1069d4c7df88484ab3ac4a65
Yaruha88 <77204010+Yaruha88@users.noreply.github.com>
2022-05-04 20:12:49 +0300

    Update Ditch.py

--- Ditch.py
+++ Ditch.py
@@ -1,18 +1,55 @@
-import Greetings
+# import Greetings
 import Storage
-from Storage import storage
 import Helpers
+import MyShoppingCart
 
-purchases = input('Что выбираешь? ')  
+Helpers.printStock(Storage.storage)
+
+purchases = input('Что выбираешь? ')
 quantity = int(input('Какое количество берешь? ')) 
-for i in Storage.storage: 
-    if i['name'] == purchases: 
-        if quantity <= i['count']: 
-            i['count'] = i['count'] - quantity 
-            print(('Ты выбрал ') + (purchases) + str(' в количестве ') + str(quantity) + str(' шт! ') + str('Осталось на складе '), + (i['count']), str('шт')) 
-        else: 
-            print('Слишком дохера, нет столько!') 
 
-Helpers.printStock(storage)
+def buy_request(x, y, z):
+    for i in x: 
+        if i['name'] == y and i['count'] >= z:
+            return ('Есть такое')
+        continue
+    return ('Нет такого')
+
+# Helpers.printStock(Storage.storage)
+otvet = buy_request(Storage.storage, purchases, quantity)
+print(otvet)
+
+print(('Ты выбрал ') + (purchases) + str(' в количестве ') + str(quantity) + str(' шт! '))
+addToShoppingCart = input('Кладём в корзину? да\нет: ')
+
+def addItems():
+    MyShoppingCart.shoppingCart.append(purchases)
+    print(purchases + ' добавлен в корзину.')
+
+if addToShoppingCart == 'да':
+    addItems()
+else:
+    print('Ну как хочешь.')
+
+pipka = input('Что дальше? \n 1. Посмотреть корзину. \n 2. Удалить товар из корзины. \n 3. Оформить покупку. \n Выбери цифру: ')
+
+def viewCart():
+    print('Вот твоя корзина: ' + str(MyShoppingCart.shoppingCart))
+
+def deleteItem():
+    MyShoppingCart.shoppingCart.clear()
+    print('Корзина очищена ')
+
+def bying():
+    viewCart()
+
+if pipka == '1':
+    viewCart()
+elif pipka == '2':
+    deleteItem()
+elif pipka == '3':
+    bying()
+else:
+    print('До скорого')
 
 
