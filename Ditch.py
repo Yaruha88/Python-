8b84da80d522fe36cd7f99e7b88ed89633ff269e
Yaruha88 <77204010+Yaruha88@users.noreply.github.com>
2022-05-06 11:22:28 +0300

    Update Ditch.py

--- Ditch.py
+++ Ditch.py
@@ -1,42 +1,17 @@
-# import Greetings
+import Greetings
 import Storage
 import Helpers
 import MyShoppingCart
 
-Helpers.printStock(Storage.storage)
-
-purchases = input('Что выбираешь? ')
-quantity = int(input('Какое количество берешь? ')) 
-
-def buy_request(x, y, z):
-    for i in x: 
-        if i['name'] == y and i['count'] >= z:
-            print('Есть такое')
-            return True
-        continue
-    print('Нет такого')
-    return False
-
-# Helpers.printStock(Storage.storage)
-otvet = buy_request(Storage.storage, purchases, quantity)
-print(otvet)
-
-print(('Ты выбрал ') + (purchases) + str(' в количестве ') + str(quantity) + str(' шт! '))
-addToShoppingCart = input('Кладём в корзину? да\нет: ')
+def viewStorage():
+    Helpers.printStock(Storage.storage)
+    
+def viewCart():
+    print('Вот твоя корзина: ' + str(MyShoppingCart.shoppingCart))
 
 def addItems():
     MyShoppingCart.shoppingCart.append(purchases)
     print(purchases + ' добавлен в корзину.')
-
-if addToShoppingCart == 'да':
-    addItems()
-else:
-    print('Ну как хочешь.')
-
-pipka = input('Что дальше? \n 1. Посмотреть корзину. \n 2. Удалить товар из корзины. \n 3. Оформить покупку. \n Выбери цифру: ')
-
-def viewCart():
-    print('Вот твоя корзина: ' + str(MyShoppingCart.shoppingCart))
 
 def deleteItem():
     MyShoppingCart.shoppingCart.clear()
@@ -52,12 +27,37 @@ def ordering():
             isOrdering = False
         print('До скорого')
 
-if pipka == '1':
+def buy_request(x, y, z):
+    for i in x: 
+        if i['name'] == y and i['count'] >= z:
+            print('Есть такое')
+            return True
+        continue
+    print('Нет такого')
+    return False
+
+if Greetings.pipka == '1':
+    viewStorage()
+elif Greetings.pipka == '2':
     viewCart()
-elif pipka == '2':
+elif Greetings.pipka == '3':
     deleteItem()
-elif pipka == '3':
+elif Greetings.pipka == '4':
     ordering()
 else:
     print('До скорого')
+
+purchases = input('Что выбираешь? ')
+quantity = int(input('Какое количество берешь? ')) 
+otvet = buy_request(Storage.storage, purchases, quantity)
+print(otvet)
+
+print(('Ты выбрал ') + (purchases) + str(' в количестве ') + str(quantity) + str(' шт! '))
+addToShoppingCart = input('Кладём в корзину? да\нет: ')
+
+if addToShoppingCart == 'да':
+    addItems()
+else:
+    print('Ну как хочешь.')
+
 
