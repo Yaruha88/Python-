0fbc2a8df94fc81321e7c530b9127fda3d400822
Yaruha88 <77204010+Yaruha88@users.noreply.github.com>
2022-05-12 13:59:09 +0300

    Update Ditch.py

--- Ditch.py
+++ Ditch.py
@@ -1,34 +1,42 @@
+from os import name
 import Greetings
 import Storage
 import Helpers
 import MyShoppingCart
-# коммент111
+
+
 
 def viewStorage():
     Helpers.printStock(Storage.storage)
-    
+
 def viewCart():
     print('Вот твоя корзина: ' + str(MyShoppingCart.shoppingCart))
-
-def addItems():
-    MyShoppingCart.shoppingCart.append(purchases)
-    print(purchases + ' добавлен в корзину.')
 
 def deleteItem():
     MyShoppingCart.shoppingCart.clear()
     print('Корзина очищена ')
 
+def addItems(purchases):
+    MyShoppingCart.shoppingCart.append(purchases)
+    print(purchases + ' добавлен в корзину.')
+
 def ordering():
+    Helpers.printStock(Storage.storage)
+    purchases = input('Что выбираешь? ')
+    quantity = int(input('Какое количество берешь? ')) 
+    otvet = checkInStorage(Storage.storage, purchases, quantity)
+    print(otvet)
+    
     isOrdering = True
     while isOrdering == True:
         zakaz = input('Заказ оформлен. Хочешь заказать еще? да\нет: ')
         if zakaz == 'да':
-            print(Helpers.printStock(Storage.storage))
+            ordering()
         else:
             isOrdering = False
         print('До скорого')
-
-def buy_request(x, y, z):
+    
+def checkInStorage(x, y, z):
     for i in x: 
         if i['name'] == y and i['count'] >= z:
             print('Есть такое')
@@ -37,28 +45,16 @@ def buy_request(x, y, z):
     print('Нет такого')
     return False
 
-if Greetings.pipka == '1':
+user_choise = Greetings.greet()
+
+if user_choise == '1':
     viewStorage()
-elif Greetings.pipka == '2':
+elif user_choise == '2':
     viewCart()
-elif Greetings.pipka == '3':
+elif user_choise == '3':
     deleteItem()
-elif Greetings.pipka == '4':
+elif user_choise == '4':
     ordering()
 else:
     print('До скорого')
-
-purchases = input('Что выбираешь? ')
-quantity = int(input('Какое количество берешь? ')) 
-otvet = buy_request(Storage.storage, purchases, quantity)
-print(otvet)
-
-print(('Ты выбрал ') + (purchases) + str(' в количестве ') + str(quantity) + str(' шт! '))
-addToShoppingCart = input('Кладём в корзину? да\нет: ')
-
-if addToShoppingCart == 'да':
-    addItems()
-else:
-    print('Ну как хочешь.')
-
 
