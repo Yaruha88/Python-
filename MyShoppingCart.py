2ca6358074f3a296d3d4546e4295fa5060bcb81c
Yaruha88 <77204010+Yaruha88@users.noreply.github.com>
2022-05-13 10:15:00 +0300

    Update MyShoppingCart.py

--- MyShoppingCart.py
+++ MyShoppingCart.py
@@ -1,1 +1,19 @@
+import Helpers
+import Storage
+
+
 shoppingCart = []
+
+def viewStorage():
+    Helpers.printStock(Storage.storage)
+
+def viewCart():
+    print('Вот твоя корзина: ' + str(shoppingCart))
+
+def deleteItem():
+    shoppingCart.clear()
+    print('Корзина очищена ')
+
+def addItems(purchases):
+    shoppingCart.append(purchases)
+    print(purchases + ' добавлен в корзину.')
