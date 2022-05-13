d46e6347db29ce774ff4ba6614770a11fe735ebd
Yaruha88 <77204010+Yaruha88@users.noreply.github.com>
2022-05-13 15:26:54 +0300

    Update Helpers.py

--- Helpers.py
+++ Helpers.py
@@ -1,4 +1,3 @@
-import Storage
 def printStock(storage):
-    for i in Storage.storage: 
+    for i in storage: 
         print(('Название: ') + (i['name']), str('в количестве '), + (i['count']), str('шт')) 
