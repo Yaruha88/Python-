e41507acdc2f40f233756919166df85ccc1d819b
Yaruha88 <77204010+Yaruha88@users.noreply.github.com>
2022-05-23 14:17:45 +0300

    Update Greetings.py

--- Greetings.py
+++ Greetings.py
@@ -2,7 +2,5 @@ def sayHello(name):
     print('Привет ' + str(name) + '. Добро пожаловать в магазин! \n 1. Посмотреть товар \n 2. Посмотреть корзину \n 3. Удалить товар из корзины \n 4. Оформить заказ')
 
 def greet():
-    name = input('Как тебя зовут? ')
-    sayHello(name)
     pipka = input('Выбери действие: ')
     return pipka
