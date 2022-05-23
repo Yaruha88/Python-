c986a4b81eab931ec03a7f7e031c3814ac296e43
Yaruha88 <77204010+Yaruha88@users.noreply.github.com>
2022-05-23 14:18:23 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
-from MyShoppingCart import deleteItem, viewCart
+import Greetings
+import Helpers
+from MyShoppingCart import deleteItem, viewCart, addItems
 import Storage
 
 
@@ -9,48 +11,26 @@ def ordering():
         quantity = int(input('Какое количество берешь? '))
         if Storage.checkOrder(Storage.storage, purchases, quantity):
             Storage.changeInStorage(Storage.storage, purchases, quantity)
+            addItems({'name':purchases, 'count':quantity})
             continueOrdering = input('Заказ оформлен. Хочешь заказать еще? да\нет: ')
         
         if continueOrdering != 'да':
             break
 
-ordering()
-
-
-
-import Greetings
-import Helpers
-from MyShoppingCart import deleteItem, viewCart
-import Storage
-
-
-def ordering():
-    Storage.viewStorage()
-    purchases = input('Что выбираешь? ')
-    quantity = int(input('Какое количество берешь? ')) 
-    otvet = Storage.checkOrder(Storage.storage, purchases, quantity)
-    otvet1 = Storage.changeInStorage(Storage.storage, purchases, quantity)
-    print(otvet)
-    print(otvet1)
-    
-    isOrdering = True
-    while isOrdering == True:
-        zakaz = input('Заказ оформлен. Хочешь заказать еще? да\нет: ')
-        if zakaz == 'да':
+def mainMenu():
+    while True:
+        user_choise = Greetings.greet()
+        if user_choise == '1':
+            Storage.viewStorage()
+        elif user_choise == '2':
+            viewCart()
+        elif user_choise == '3':
+            deleteItem()
+        elif user_choise == '4':
             ordering()
         else:
-            isOrdering = False
-        print('До скорого')
-    
-user_choise = Greetings.greet()
+            print('До скорого')
 
-if user_choise == '1':
-    Storage.viewStorage()
-elif user_choise == '2':
-    viewCart()
-elif user_choise == '3':
-    deleteItem()
-elif user_choise == '4':
-    ordering()
-else:
-    print('До скорого')
+name = input('Как тебя зовут? ')
+Greetings.sayHello(name)
+mainMenu()
