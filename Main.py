6be1e4d2fe4f9068cce2843e9ae7e396d486f7a6
Yaruha88 <yaruha88@gmail.com>
2022-06-28 12:35:49 +0300

    ...

--- Main.py
+++ Main.py
@@ -4,30 +4,64 @@ import Greetings
 import Helpers
 from MyShoppingCart import deleteItem, viewCart, addItems
 import Storage
-
+import MyShoppingCart
 
 bot = telebot.TeleBot('5202629983:AAHB0cUjCLqJEz8rWs7I-_WHWGLkdYniAX8')
 @bot.message_handler(commands=['start'])
 def start_message(message):
     # bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
     markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-    item1 = types.KeyboardButton('1. Посмотреть товар')
-    item2 = types.KeyboardButton('2. Посмотреть корзину')
-    markup.add(item1)
-    markup.add(item2)
-    bot.send_message(message.chat.id, 'Нажми: \nФакт для получения интересного факта\nПоговорка — для получения мудрой цитаты ',  reply_markup=markup)
+    markup.row('1. Посмотреть товар', '2. Посмотреть корзину')
+    markup.row('3. Удалить товар из корзины', '4. Оформить заказ')
+    # item1 = types.KeyboardButton('1. Посмотреть товар')
+    # item2 = types.KeyboardButton('2. Посмотреть корзину')
+    # markup.add(item1)
+    # markup.add(item2)
+    # bot.send_message(message.chat.id, 'Привет! Добро пожаловать! Нажми: \n1. Посмотреть товар\n2. Посмотреть корзину',  reply_markup=markup)
+    bot.send_message(message.chat.id, f"""
+    Приветствуем Вас, *{message.from_user.first_name}*
+    Добро пожаловать! Нажми:
+    1. Посмотреть товар
+    2. Посмотреть корзину
+    3. Удалить товар из корзины
+    4. Оформить заказ""", parse_mode="Markdown", reply_markup=markup)
     # bot.send_message(message.chat.id, "{message.chat.username}, Добро пожаловать в магазин! \n 1. Посмотреть товар \n 2. Посмотреть корзину \n 3. Удалить товар из корзины \n 4. Оформить заказ", + message.text)
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if message.text.strip() == '1. Посмотреть товар':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-        cancel = types.KeyboardButton('Назад')
-        markup.add(cancel)
+        # markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+        # cancel = types.KeyboardButton('Назад')
+        # markup.add(cancel)
         for i in Storage.storage: 
             answer = ('Название: ') + (i['name']), str('в количестве '), + (i['count']), str('шт')
-            bot.send_message(message.chat.id, answer, reply_markup=markup) 
-        # bot.send_message(message.chat.id, 'Привет')
+            bot.send_message(message.chat.id, answer) 
+    elif message.text.strip() == '2. Посмотреть корзину':
+        if len(MyShoppingCart.shoppingCart) == 0:
+           bot.send_message(message.chat.id, 'Корзина пустая')
+        else:
+            bot.send_message(message.chat.id, 'В корзине: ')
+            for i in Storage.storage:
+                incart = ('Название: ') + (i['name']), str('в количестве '), + (i['count']), str('шт')
+                bot.send_message(message.chat.id, incart)
+        # bot.send_message(message.chat.id, 'Нажми: \n1. Посмотреть товар\n2. Посмотреть корзину')
+    elif message.text.strip() == '3. Удалить товар из корзины':
+        MyShoppingCart.shoppingCart.clear()
+        bot.send_message(message.chat.id, 'Корзина очищена ')
+    elif message.text.strip() == '4. Оформить заказ':
+        Storage.viewStorage()
+        purchases = ('Что выбираешь? ')
+        bot.send_message(message.chat.id, purchases)
+        quantity = ('Какое количество берешь? ')
+        bot.send_message(message.chat.id, quantity)
+        if Storage.checkOrder(Storage.storage, purchases, quantity):
+            Storage.changeInStorage(Storage.storage, purchases, quantity)
+            addItems({'name':purchases, 'count':quantity})
+            continueOrdering = ('Заказ оформлен. Хочешь заказать еще? да\нет: ')
+            bot.send_message(message.chat.id, continueOrdering)
+        if message.text.strip() == 'нет':
+            bot.send_message(message.chat.id, 'До свидания')
+
         
         
 
