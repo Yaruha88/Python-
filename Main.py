8c6547613f033c9e2eb6932cedb448960909794f
Yaruha88 <yaruha88@gmail.com>
2022-07-04 15:10:02 +0300

    1212

--- Main.py
+++ Main.py
@@ -1,25 +1,20 @@
+from os import name
+from pydoc import Helper
 from telebot import types
-import telebot    
-import Greetings
+import telebot
 import Helpers
-from MyShoppingCart import deleteItem, viewCart, addItems
 import Storage
 import MyShoppingCart
+import copy
 ODERING = ''
-CART = {}
+PRODUCT = dict()
 
 bot = telebot.TeleBot('5202629983:AAHB0cUjCLqJEz8rWs7I-_WHWGLkdYniAX8')
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    # bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
     markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
     markup.row('1. Посмотреть товар', '2. Посмотреть корзину')
     markup.row('3. Удалить товар из корзины', '4. Оформить заказ')
-    # item1 = types.KeyboardButton('1. Посмотреть товар')
-    # item2 = types.KeyboardButton('2. Посмотреть корзину')
-    # markup.add(item1)
-    # markup.add(item2)
-    # bot.send_message(message.chat.id, 'Привет! Добро пожаловать! Нажми: \n1. Посмотреть товар\n2. Посмотреть корзину',  reply_markup=markup)
     bot.send_message(message.chat.id, f"""
     Приветствуем Вас, *{message.from_user.first_name}*
     Добро пожаловать! Нажми:
@@ -27,61 +22,77 @@ def start_message(message):
     2. Посмотреть корзину
     3. Удалить товар из корзины
     4. Оформить заказ""", parse_mode="Markdown", reply_markup=markup)
-    # bot.send_message(message.chat.id, "{message.chat.username}, Добро пожаловать в магазин! \n 1. Посмотреть товар \n 2. Посмотреть корзину \n 3. Удалить товар из корзины \n 4. Оформить заказ", + message.text)
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+    global ODERING
+    global PRODUCT
+
     if message.text.strip() == '1. Посмотреть товар':
-        # markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-        # cancel = types.KeyboardButton('Назад')
-        # markup.add(cancel)
         for i in Storage.storage: 
-            answer = ('Название: ') + (i['name']), str('в количестве '), + (i['count']), str('шт')
+            answer = ('Название: ') + (i['name']) + str(' в количестве ') + (i['count']) + str(' шт')
             bot.send_message(message.chat.id, answer) 
     elif message.text.strip() == '2. Посмотреть корзину':
-        if len(MyShoppingCart.shoppingCart) == 0:
-           bot.send_message(message.chat.id, 'Корзина пустая')
-        else:
-            bot.send_message(message.chat.id, 'В корзине: ')
-            for i in Storage.storage:
-                incart = ('Название: ') + (i['name']), str('в количестве '), + (i['count']), str('шт')
-                bot.send_message(message.chat.id, incart)
-        # bot.send_message(message.chat.id, 'Нажми: \n1. Посмотреть товар\n2. Посмотреть корзину')
+        cart = MyShoppingCart.viewCart()
+        for i in cart: 
+            answer = ('Название: ') + (i['name']) + str(' в количестве ') + str(i['count']) + str(' шт')
+            bot.send_message(message.chat.id, answer) 
     elif message.text.strip() == '3. Удалить товар из корзины':
         MyShoppingCart.shoppingCart.clear()
         bot.send_message(message.chat.id, 'Корзина очищена ')
     elif message.text.strip() == '4. Оформить заказ':
         for i in Storage.storage: 
-            answer = ('Название: ') + (i['name']), str('в количестве '), + (i['count']), str('шт')
+            answer = ('Название: ') + (i['name']) + str(' в количестве ') + str(i['count']) + str(' шт')
             bot.send_message(message.chat.id, answer) 
         bot.send_message(message.chat.id, 'Что выбираешь? ')
-        global ODERING
         ODERING = 'name'
-        global CART
-        CART = {'name', 'count'}
-        ODERING == 'name'
-        CART['name'] = message.text.strip()
+    elif ODERING == 'name':
+        Check = Helpers.checkInStorage(ODERING['check'], ODERING['name_item'])
+        if Check == True:
+            answer = ('Отлично!')
+            bot.send_message(message.chat.id, answer)
+        else:
+            answer = ('Неверно введено название товара. Введи название правильно.')
+            bot.send_message(message.chat.id, answer)
+        # написать функцию проверки имени типа как в Storage и вывести сюда как в переменной Change
+        PRODUCT['name'] = message.text.strip()
         bot.send_message(message.chat.id, 'Введи количество')
         ODERING = 'count'
     elif ODERING == 'count':
-        CART['count'] = message.text.strip()
-        ODERING = False
-        bot.send_message(message.chat.id, CART)
+        PRODUCT['count'] = int(message.text.strip())
+        MyShoppingCart.addItems(copy.copy(PRODUCT))
+        Change = Storage.changeInStorage(Storage.storage, PRODUCT['name'], PRODUCT['count'])
+        if Change == True:
+            answer = ('Ты заказал: ') + (PRODUCT['name']) + str(' в количестве ') + str((PRODUCT['count'])) + str(' шт')
+            bot.send_message(message.chat.id, answer)
+            ODERING = False
+        else:
+            answer = ('Неправильное количество')
+            bot.send_message(message.chat.id, answer)
+           
+        # Прочитать "Копирование по значению, и по ссылке, в чем отличие"
         
-# @bot.message_handler(content_types=['text'])
-# def handle_text(message):
-#     bot.send_message(message.chat.id, 'Ты выбрал?' + message.text)
+while True:  # Запускаем бота
+    try:
+        bot.polling(none_stop=True)
+    except OSError:
+        bot.polling(none_stop=True)
 
-        # quantity = ('Какое количество берешь? ')
-        # bot.send_message(message.chat.id, quantity)
-        # if Storage.checkOrder(Storage.storage, purchases, quantity):
-        #     Storage.changeInStorage(Storage.storage, purchases, quantity)
-        #     addItems({'name':purchases, 'count':quantity})
-        #     continueOrdering = ('Заказ оформлен. Хочешь заказать еще? да\нет: ')
-        #     bot.send_message(message.chat.id, continueOrdering)
-        # if message.text.strip() == 'нет':
-        #     bot.send_message(message.chat.id, 'До свидания')
+# def order():
+#     purchases = input('Что выбираешь? ')
+#     quantity = int(input('Какое количество берешь? ')) 
+#     otvet = checkInStorage(Storage.storage, purchases, quantity)
+#     print(otvet)
 
+#     print(('Ты выбрал ') + (purchases) + str(' в количестве ') + str(quantity) + str(' шт! '))
+#     addToShoppingCart = input('Кладём в корзину? да\нет: ')
+
+#     if addToShoppingCart == 'да':
+#         addItems()
+#     else:
+#         print('Ну как хочешь.')
+#         Greetings.sayHello(name)
+#     return purchases
         
         
 
@@ -116,29 +127,4 @@ def handle_text(message):
 # name = input('Как тебя зовут? ')
 # Greetings.sayHello(name)
 # mainMenu()
-
-while True:  # Запускаем бота
-    try:
-        bot.polling(none_stop=True)
-    except OSError:
-        bot.polling(none_stop=True)
-
-# def order():
-#     purchases = input('Что выбираешь? ')
-#     quantity = int(input('Какое количество берешь? ')) 
-#     otvet = checkInStorage(Storage.storage, purchases, quantity)
-#     print(otvet)
-
-#     print(('Ты выбрал ') + (purchases) + str(' в количестве ') + str(quantity) + str(' шт! '))
-#     addToShoppingCart = input('Кладём в корзину? да\нет: ')
-
-#     if addToShoppingCart == 'да':
-#         addItems()
-#     else:
-#         print('Ну как хочешь.')
-#         Greetings.sayHello(name)
-#     return purchases
-
-
-
 
